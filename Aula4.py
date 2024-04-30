c6a29f853ccb98261b5faf1cb2254c64e11c9d50
rerevelation <rerevelation12@gmail.com>
2024-04-30 10:36:52 -0300

    first commit

---
+++ Aula4.py
@@ -0,0 +1,11 @@
+minha_lista = ['python',15,'false']
+
+
+minha_lista[1] = ""
+
+
+print(minha_lista )
+
+#insert - ordenar
+#append - add um novo intem sem ordem
+# remove - remover
---
+++ script.js
@@ -0,0 +1,5 @@
+
+function comprar() {
+               alert("Produto adicionado ao carrinho!");
+           }
+           
